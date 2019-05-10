2d0bf44877da9d3ec27f574108353ddc62b62e0b
chungfung <fy_java@163.com>
2019-05-10 10:50:52 +0800

    Initial commit

---
+++ job-springboot/src/main/java/com/hgsoft/JobSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.hgsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSpringbootApplication.class, args);
+	}
+
+}
